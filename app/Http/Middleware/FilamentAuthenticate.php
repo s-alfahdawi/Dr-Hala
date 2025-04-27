d0691270b04a91d4a19b0e9eb7795d93d8af96fd
s-alfahdawi <39900828+s-alfahdawi@users.noreply.github.com>
2025-04-27 17:00:50 +0300

    login fix

--- app/Http/Middleware/FilamentAuthenticate.php
+++ app/Http/Middleware/FilamentAuthenticate.php
@@ -4,14 +4,15 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class FilamentAuthenticate
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if (! auth()->check()) {
-            abort(403);
+        if (! Auth::check()) {
+            return redirect()->route('filament.admin.auth.login'); // لاحظ هنا توجيه
         }
 
         return $next($request);
