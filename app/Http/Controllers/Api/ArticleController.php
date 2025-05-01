2d9f0ee6f6b67e81bef87c72405f9e8da5604295
s-alfahdawi <39900828+s-alfahdawi@users.noreply.github.com>
2025-05-01 14:23:39 +0300

    cms api fix

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -1,19 +1,31 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
 use App\Models\Article;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Str;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        return Article::latest()->get();
-    }
+        $articles = Article::all()->map(function ($article) {
+            return [
+                'id' => $article->id,
+                'title' => $article->title,
+                'content' => $article->content,
+                'images' => collect($article->images)->map(function ($image) {
+                    return Str::startsWith($image, ['http://', 'https://'])
+                        ? $image
+                        : asset('storage/' . $image);
+                })->toArray(),
+                'created_at' => $article->created_at,
+                'updated_at' => $article->updated_at,
+            ];
+        });
 
-    public function show($id)
-    {
-        return Article::findOrFail($id);
+        return response()->json($articles);
     }
 }
